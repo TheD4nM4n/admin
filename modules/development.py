c63499115d08eacbc81842a81e6cd9774d82a992
thed4nm4n <thed4nm4n@gmail.com>
2020-12-31 16:22:24 -0500

    -mute: keep people from talking

--- modules/development.py
+++ modules/development.py
@@ -3,17 +3,51 @@ from discord.ext import commands
 import json
 
 
+def load_configuration():
+    # Returns the configuration stored on disk.
+    with open("./data/serverconfig.json", "r") as stored_config:
+        return json.load(stored_config)
+
+
+def save_configuration(config):
+    with open("./data/serverconfig.json", "w") as stored_config:
+        # This writes the configuration with the changes made to the disk.
+        json.dump(config, stored_config, indent=4)
+        stored_config.truncate()
+
+
 class DevelopmentModule(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        with open("./data/rr.json", "r") as rr:
-            self.links = json.load(rr)
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        pass
+    async def on_message(self, message):
 
-            
+        # Loads mute configuration
+        config = load_configuration()
+        guild_config = config[f"{message.guild.id}"]["mute"]
+
+        # Checks if member is muted and deletes message if so
+        if str(message.author.id) in guild_config["muted-members"]:
+            return await message.delete()
+
+    @commands.command()
+    async def mute(self, ctx, intent=None, member: discord.Member = None):
+        config = load_configuration()
+        guild_config = config[f"{ctx.guild.id}"]["mute"]
+
+        if intent:
+            if intent.lower() == "add":
+                guild_config["muted-members"].append(f"{member.id}")
+                save_configuration(config)
+                return await ctx.message.add_reaction("✅")
+            elif intent.lower() == "remove":
+                if str(member.id) in guild_config["muted-members"]:
+                    guild_config["muted-members"].remove(str(member.id))
+                    save_configuration(config)
+                    return await ctx.message.add_reaction("✅")
+
+
 def setup(bot):
     bot.add_cog(DevelopmentModule(bot))
