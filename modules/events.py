0afa8a75c622e07dffa4e0ecac5876b83285425a
thed4nm4n <thed4nm4n@gmail.com>
2021-01-14 13:41:19 -0500

    -giveaway: draw winners for prizes

---
+++ modules/events.py
@@ -0,0 +1,33 @@
+from discord import Role
+from random import choice
+from discord.ext import commands
+
+
+class EventsModule(commands.Cog):
+
+    def __init__(self, bot):
+
+        # Discord.py things
+        self.bot = bot
+
+    @commands.command(description="A powerful set of tools for running giveaways.")
+    @commands.has_permissions(administrator=True)
+    async def giveaway(self, ctx, intent=None, role: Role = None) -> None:
+
+        if intent:
+
+            if intent.lower() == "draw":
+
+                if role:
+                    giveaway_winner = choice(role.members)
+                    await ctx.send(f"Your winner is {giveaway_winner.mention}!")
+                    return
+
+                else:
+                    giveaway_winner = choice(ctx.guild.members)
+                    await ctx.send(f"Your winner is {giveaway_winner.mention}!")
+                    return
+
+
+def setup(bot):
+    bot.add_cog(EventsModule(bot))
