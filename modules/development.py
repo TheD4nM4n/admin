bf506e26f368e88e7b8b06c24f3cf3174b35515c
thed4nm4n <thed4nm4n@gmail.com>
2021-04-16 01:08:16 -0400

    Optimized imports

--- modules/development.py
+++ modules/development.py
@@ -1,16 +1,10 @@
-import discord
 from discord.ext import commands
-import json
-from core import load_configuration, save_configuration
 
 
 class DevelopmentModule(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener('on_ready')
-    async def loaded_message(self):
         print("'Development' module loaded.")
 
 
