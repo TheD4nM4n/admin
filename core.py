9cff323ba5aa92ee85f45744ff0b012d0ff455e4
thed4nm4n <thed4nm4n@gmail.com>
2021-04-16 01:08:04 -0400

    Unified configuration system

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ import os
 import json
 
 from discord import Embed, File, Guild, Activity, ActivityType
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import Intents
 
 
@@ -38,17 +38,17 @@ class AdminBot(commands.Bot):
                          intents=kwargs['intents'],
                          activity=Activity(type=ActivityType.listening, name="-help"))
 
+        self.remove_command("help")
+        self.config = load_configuration()
+        self.save_config.start()
+
+    async def on_ready(self):
         for module in os.listdir('./modules'):
             if module.endswith('.py'):
                 self.load_extension(f'modules.{module[:-3]}')
 
-        self.remove_command("help")
-
-    async def on_ready(self):
-        config = load_configuration()
-
         for guild in self.guilds:
-            if str(guild.id) not in config.keys():
+            if str(guild.id) not in self.config.keys():
                 # Loads the default config and modifies it to fit the server
                 default_config = load_default_configuration()
                 if guild.system_channel:
@@ -56,48 +56,47 @@ class AdminBot(commands.Bot):
                 default_config["name"] = guild.name
 
                 # Adds the server to the config, with the above configuration
-                config[f"{guild.id}"] = default_config
+                self.config[f"{guild.id}"] = default_config
 
         # Writes the new config to disk
-        save_configuration(config)
+        save_configuration(self.config)
 
-    @staticmethod
-    async def on_guild_join(guild: Guild) -> None:
-
-        # Loads the serverconfig.json file and looks for the server in the json
-        config = load_configuration()
+    async def on_guild_join(self, guild: Guild) -> None:
 
         # If the server isn't in the config file, it loads the default config and modifies it to fit the server
-        if guild.id not in config.keys():
+        if str(guild.id) not in self.config.keys():
             default_config = load_default_configuration()
             default_config["greetings"]["channel"] = guild.system_channel.id
             default_config["name"] = guild.name
 
             # Adds the server to the config, with the above configuration
-            config[f"{guild.id}"] = default_config
+            self.config[f"{guild.id}"] = default_config
 
             # Writes the new config to disk
-            save_configuration(config)
+            save_configuration(self.config)
+
+    @tasks.loop(minutes=1.0)
+    async def save_config(self):
+        save_configuration(self.config)
 
 
-bot = AdminBot(command_prefix="-",
-               intents=Intents.all())
+admin = AdminBot(command_prefix="-",
+                 intents=Intents.all())
 
 
-@bot.command(description="Reloads the specified module, or all of them if no module is specified.")
+@admin.command(description="Reloads the specified module, or all of them if no module is specified.")
 @commands.has_permissions(administrator=True)
 async def reload(ctx: commands.Context, module_name=None):
-
     if module_name is None:
 
-        active_modules = [extension for extension in bot.extensions]
+        active_modules = [extension for extension in admin.extensions]
 
         for module_title in active_modules:
-            bot.unload_extension(module_title)
+            admin.unload_extension(module_title)
 
         for module_title in os.listdir("./modules"):
             if module_title.endswith(".py"):
-                bot.load_extension(f"modules.{module_title[:-3]}")
+                admin.load_extension(f"modules.{module_title[:-3]}")
 
         await ctx.message.add_reaction("✅")
 
@@ -105,33 +104,33 @@ async def reload(ctx: commands.Context, module_name=None):
 
         full_module_name = f'modules.{module_name}'
 
-        if full_module_name in bot.extensions:
-            bot.unload_extension(full_module_name)
-            bot.load_extension(full_module_name)
+        if full_module_name in admin.extensions:
+            admin.unload_extension(full_module_name)
+            admin.load_extension(full_module_name)
             return await ctx.message.add_reaction("✅")
 
-        bot.load_extension(full_module_name)
+        admin.load_extension(full_module_name)
         await ctx.message.add_reaction("✅")
 
 
-@bot.command(description="Lists all modules.")
+@admin.command(description="Lists all modules.")
 @commands.has_permissions(administrator=True)
 async def modules(ctx: commands.Context):
     active_modules = ''
-    for bot_module in bot.extensions:
-        active_modules += f"{bot_module[8:]}\n"
+    for admin_module in admin.extensions:
+        active_modules += f"{admin_module[8:]}\n"
 
     inactive_modules = ''
-    for bot_module in os.listdir("./modules"):
-        if bot_module.endswith(".py"):
-            if f"modules.{bot_module[:-3]}" not in bot.extensions:
-                inactive_modules += f"{bot_module[:-3]}\n"
+    for admin_module in os.listdir("./modules"):
+        if admin_module.endswith(".py"):
+            if f"modules.{admin_module[:-3]}" not in admin.extensions:
+                inactive_modules += f"{admin_module[:-3]}\n"
 
     embed = Embed(title="Module List",
                   description="This is where you can see everything I do!")
 
     if len(active_modules) > 0:
-        embed.add_field(name=f"There are **{len(bot.extensions)}** active modules:",
+        embed.add_field(name=f"There are **{len(admin.extensions)}** active modules:",
                         value=active_modules)
 
     if len(inactive_modules) > 0:
@@ -141,12 +140,12 @@ async def modules(ctx: commands.Context):
     await ctx.send(embed=embed)
 
 
-@bot.command(description="Enables a module.")
+@admin.command(description="Enables a module.")
 @commands.has_permissions(administrator=True)
 async def enable(ctx: commands.Context, module_name):
-    if f"modules.{module_name}" not in bot.extensions:
+    if f"modules.{module_name}" not in admin.extensions:
         if f"{module_name.lower()}.py" in os.listdir("./modules"):
-            bot.load_extension(f"modules.{module_name.lower()}")
+            admin.load_extension(f"modules.{module_name.lower()}")
             await ctx.message.add_reaction("✅")
         else:
             await ctx.message.add_reaction("❌")
@@ -155,27 +154,27 @@ async def enable(ctx: commands.Context, module_name):
         return await ctx.send(f"That module is already loaded!")
 
 
-@bot.command(description="Disables a module.")
+@admin.command(description="Disables a module.")
 @commands.has_permissions(administrator=True)
 async def disable(ctx: commands.Context, module_name):
-    if f"modules.{module_name.lower()}" in bot.extensions:
-        bot.remove_cog(module_name.lower())
-        bot.unload_extension(f'modules.{module_name.lower()}')
+    if f"modules.{module_name.lower()}" in admin.extensions:
+        admin.remove_cog(module_name.lower())
+        admin.unload_extension(f'modules.{module_name.lower()}')
         await ctx.message.add_reaction("✅")
     else:
         await ctx.message.add_reaction("❌")
         await ctx.send("Sorry, that module either doesn't exist, or is already disabled.")
 
 
-@bot.group(name="help", description="Displays all Admin commands.", invoke_without_command=True)
+@admin.group(name="help", description="Displays all Admin commands.", invoke_without_command=True)
 async def help_command(ctx: commands.Context):
     file = File(fp="./assets/vgclove.png")
 
     embed = Embed(title="Help", description="Thank you for seeing what I can do!",
                   color=0xff0000)
     embed.set_thumbnail(url="attachment://vgclove.png")
-    for command in bot.commands:
-        command_data = bot.get_command(command.name)
+    for command in admin.commands:
+        command_data = admin.get_command(command.name)
         if command_data.checks:
             pass
         else:
@@ -195,8 +194,8 @@ async def moderator_help(ctx: commands.Context):
     embed = Embed(title="Help", description="Thank you for seeing what I can do (for administrators)!",
                   color=0xff0000)
     embed.set_thumbnail(url="attachment://vgclove.png")
-    for command in bot.commands:
-        command_data = bot.get_command(command.name)
+    for command in admin.commands:
+        command_data = admin.get_command(command.name)
         if not command_data.checks:
             pass
         else:
@@ -209,4 +208,4 @@ async def moderator_help(ctx: commands.Context):
 
 
 if __name__ == "__main__":
-    bot.run(load_token())
+    admin.run(load_token())
