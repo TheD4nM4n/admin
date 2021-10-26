97b1c8ecf7d03de67fc9790c921d1c9b1c5ded63
Daniel <thed4nm4n@gmail.com>
2021-10-25 22:25:48 -0400

    Fixed syntax

---
+++ events/interactionCreate.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // Command execution code
+    name: 'interactionCreate',
+
+    execute(interaction) {
+
+        if (!interaction.isCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            interaction.reply({ content: 'There was an error while running this command, please try again.', ephemeral: true });
+        
+        }
+    }
+}
