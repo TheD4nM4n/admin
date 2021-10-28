0fc897c7fdedd2fa98dfbc2ac2f05329b6477fe3
Daniel <thed4nm4n@gmail.com>
2021-10-28 01:00:35 -0400

    Introduced automatic config generation on startup

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,57 @@
+const fs = require('fs');
+const defaultConfig = {
+    "name": null,
+    "greetings": {
+        "enabled": true,
+        "channel": null
+    },
+    "reaction-roles": {
+        "enabled": true
+    },
+    "chat-filter": {
+        "enabled": true,
+        "log-channel": null,
+        "use-default-list": true,
+        "custom-words": [],
+        "whitelisted-channels": [],
+        "whitelisted-members": []
+    },
+    "mute": {
+        "enabled": true,
+        "muted-members": []
+    }
+}
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Successfully logged in as ${client.user.username}.`);
+
+        fs.readFile('./data/guildConfig.json', (err, rawConfig) => {
+            const config = JSON.parse(rawConfig);
+
+            client.guilds.cache.forEach((guild) => {
+
+                if (config[`${guild.id}`] === undefined) {
+                    const guildConfig = JSON.parse(JSON.stringify(defaultConfig));
+
+                    if (guild.systemChannel) {
+                        guildConfig['greetings']['channel'] = guild.systemChannel.id
+                    }
+
+                    guildConfig['name'] = guild.name
+                    config[`${guild.id}`] = guildConfig
+                }
+
+                // TODO: Asynchronous non-corrupting config writing
+                const writableConfig = JSON.stringify(config, null, 2);
+                fs.writeFileSync('./data/guildConfig.json', writableConfig, err => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+            })    
+        })
     }
 }
