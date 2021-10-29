0d4cbe15b58d9f4d38afc62276d22865decf6d8b
Daniel <thed4nm4n@gmail.com>
2021-10-28 23:13:45 -0400

    Finished help command

--- commands/help.js
+++ commands/help.js
@@ -14,11 +14,27 @@ module.exports = {
             description: `**Admin now uses slash commands!**
             To view the list of possible commands, just
             type a forward slash (/) to view the possible
-            options! For answers to some frequently asked
-            questions, try "/faq"!`,
+            options!`,
             thumbnail: {
                 url: 'attachment://vgcyes.png',
-            }
+            },
+            fields: [
+                {
+                    name: 'What is a slash command?',
+                    value: `Slash Commands are a new, more intuitive way to interact with Admin! Commands are listed natively within the Discord client, and can be accessed by typing a forward slash.`,
+                    inline: false
+                },
+                {
+                    name: 'Can I still use a dash?',
+                    value: `Unfortunately, no. dash commands have been removed with the advent of slash commands. All of the original functionality (and more!) is still intact, so you aren\t missing out on anything!`,
+                    inline: false
+                },
+                {
+                    name: 'What is all this talk about "Jay-S"?',
+                    value: `JS is short for JavaScript, the programming language that is now the home of Admin! The rewrite of Admin in JavaScript comes features like slash commands with more soon to come.`,
+                    inline: false
+                }
+            ]
         };
         return await interaction.reply({ embeds: [embed], files: [file] });
     }
