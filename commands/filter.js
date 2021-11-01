0ba74c75a19c8055e0f380bdd26d6366a20b70c8
Daniel <thed4nm4n@gmail.com>
2021-11-01 16:19:24 -0400

    Initiated development on filter command

--- commands/filter.js
+++ commands/filter.js
@@ -1,13 +1,29 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ChannelType } = require('discord-api-types/v9');
 const { Permissions } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('filter')
-    .setDescription('Configuration for Admin\'s built-in chat filter.'),
+    .setDescription('Configuration for Admin\'s built-in chat filter.')
+    .addSubcommandGroup(subcommandGroup => 
+        subcommandGroup
+        .setName('log')
+        .setDescription('Change settings for the built-in chat filter.'))
+        .addSubcommand(subcommand =>
+            subcommand
+            .setName('channel')
+            .setDescription('Sets the channel for log messages to be sent to.'))
+            .addChannelOption(builder =>
+                builder
+                .addChannelType(ChannelType.GuildText)
+                .setName('log-channel')
+                .setDescription('The channel to send infraction logs to.')
+                .setRequired(true)
+                ),
     async execute(interaction) {
         if (interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-            await interaction.reply({ content: 'Work in progress. Come back later!' })
+            return await interaction.reply({ content: 'Work in progress. Try again later!' });
         } else {
             return await interaction.reply({ content: 'You don\'t have permission to use this command!', ephemeral: true })
         };
